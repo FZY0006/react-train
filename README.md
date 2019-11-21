d5f127604c3dfff0d2f9c60cfc3218154c6019ac
FZY0007 <1109550982@qq.com>
2019-11-21 14:24:32 +0800

    11.21

--- README.md
+++
@@ -1,4 +0,0 @@
-﻿# react练习
-# 1）目标：https://fzy0006.github.io/react-train/dist/index.html
-#2）井字棋：https://fzy0006.github.io/react-train/chess.html
-
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'lodash';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-// 渲染组件
-ReactDOM.render(
-    <App />,
-    document.getElementById('container')
-);
